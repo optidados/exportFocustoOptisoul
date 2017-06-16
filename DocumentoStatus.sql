e144cb0c269d805c6e01b54f9a2151254a81bf75
murilo-cassiani <murilo.cassiani@gmail.com>
2017-06-16 18:20:29 -0300

    DocumentoStatus
    Contém os diversos status pelo qual o documento passa

---
+++ DocumentoStatus.sql
@@ -0,0 +1,14 @@
+drop table if exists documentostatus;
+
+create table documentostatus
+(
+	CodigoDocumento	int, --not null
+	CodigoDocumentoItem	int, --null
+	CodigoContatoResponsavel int, --null
+	Operacao varchar(255), --null
+	StatusOriginal varchar(255), --null
+	StatusFinalizado varchar(255), --null
+	DataHoraEmissao	datetime, --not null
+	CodigoUsuarioAlterou int, --null
+	CodigoDocumentoCaixa int --null
+);
