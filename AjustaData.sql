c378e6fcba0c50792101d8a630cff836515e6ca6
murilo-cassiani <murilo.cassiani@gmail.com>
2017-11-22 15:50:41 -0200

    Problema data FOCUS
    Corrigi exatamente para a data que dava problema: 01/01/1000. Datas antes disso o programa do Andrea não converte.

--- AjustaData.sql
+++ AjustaData.sql
@@ -1,44 +1,39 @@
 update Agenda as a
 	set a."Data" = CAST(NULL as date)
-	where a."Data" < DATE'1900-01-01'
-		or a."Data" > DATE'2020-01-01';
+	where a."Data" < DATE'1000-01-01';
 
 update Busta as b
 	set b."Data prevista consegna" = CAST(NULL as date)
-	where b."Data prevista consegna" < DATE'1900-01-01'
-		or b."Data prevista consegna" > DATE'2020-01-01';
+	where b."Data prevista consegna" < DATE'1000-01-01';
 
 update Clienti as c
 	set c."Data nascita" = CAST(NULL as date)
-	where c."Data nascita" < DATE'1900-01-01'
-		or c."Data nascita" > DATE'2020-01-01';
+	where c."Data nascita" < DATE'1000-01-01';
 
 update FatturaClienti as fc
 	set fc."Data" = CAST(NULL as date)
-	where fc."Data" < DATE'1900-01-01'
-		or fc."Data" > DATE'2020-01-01';
+	where fc."Data" < DATE'1000-01-01';
 
 update Movimenti as m
 	set m."Data ddt" = CAST(NULL as date)
-	where m."Data ddt" < DATE'1900-01-01'
-		or m."Data ddt" > DATE'2020-01-01';
+	where m."Data ddt" < DATE'1000-01-01';
 
 update Occhiali as o
 	set o."Data prescrizione" = CAST(NULL as date)
-	where o."Data prescrizione" < DATE'1900-01-01'
-		or o."Data prescrizione" > DATE'2020-01-01';
+	where o."Data prescrizione" < DATE'1000-01-01';
 
 update Occhiali as o
 	set o."Richiamo data1" = CAST(NULL as date)
-	where o."Richiamo data1" < DATE'1900-01-01'
-		or o."Richiamo data1" > DATE'2020-01-01';
+	where o."Richiamo data1" < DATE'1000-01-01';
 
 update Occhiali as o
 	set o."Richiamo data2" = CAST(NULL as date)
-	where o."Richiamo data2" < DATE'1900-01-01'
-		or o."Richiamo data2" > DATE'2020-01-01';
+	where o."Richiamo data2" < DATE'1000-01-01';
 
 update Rataclienti as rc
 	set rc."Data" = CAST(NULL as date)
-	where rc."Data" < DATE'1900-01-01'
-		or rc."Data" > DATE'2020-01-01';
+	where rc."Data" < DATE'1000-01-01';
+
+update "cf-e_" as t
+	set t."prezzo ivato" = 0
+	where CAST("prezzo ivato" as varchar(50)) = 'NAN';
